58f345e36e0dee9e5af38098fd7dc3e84869c41e
Ivan Shyriaiev <ivshiryaev1999@gmail.com>
2023-12-26 02:48:51 +0100

    added links to the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
+import links from '@/data/footerLinks'
+import Link from 'next/link'
+
 function Footer() {
 	return (
-		<footer className='p-6 lg:p-8 flex flex-col gap-4 bg-white rounded-2xl text-center'>
-			<p className='text-sm text-gray-400'>kupazsklep@gmail.com</p>
+		<footer className='p-6 lg:p-8 flex flex-col gap-4 bg-white rounded-2xl text-center text-gray-400'>
+			<Link href={`mailto:kupazsklep@gmail.com`} className='text-sm'>kupazsklep@gmail.com</Link>
+			<nav>
+				<ul>
+					{links.map(link => (
+						<li key={link.href}><Link className='text-sm hover:text-dark' href={`/${link.href}`}>{link.title}</Link></li>
+					))}
+				</ul>
+			</nav>
+			<p className='text-[0.75rem]'>Kupaz. Wszystkie prawa zastrzeżone</p>
 		</footer>
 	)
 }

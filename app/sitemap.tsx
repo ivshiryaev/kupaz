b495a24ec8b0f53a1252d95416a7b8bee9ea0305
Ivan Shyriaiev <ivshiryaev1999@gmail.com>
2023-12-26 03:13:44 +0100

    added routes such as: /, /smaki, /contact to sitemap.tsx

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -3,22 +3,47 @@ import { getSmakSlug } from '@/lib/utils'
 
 import { MetadataRoute } from 'next'
 
+import navLinks from '@/data/navLinks'
+
 export default async function sitemap(): MetadataRoute.Sitemap {
   const response = await getSmaki()
   if(!response) return null
 
-  const data = JSON.parse(response)
-
+  const smaki = JSON.parse(response)
 
   let urlPrefix = 'http://localhost:3000';
-  // if (process.env.NODE_ENV !== 'production') {
-  //   urlPrefix = 'https://kupaz.pl';
-  // }
+  if (process.env.NODE_ENV !== 'production') {
+    urlPrefix = 'https://kupaz.pl';
+  }
 
-  return data.map((smak) => ({
-    url: `${urlPrefix}/Oferta/${getSmakSlug(smak)}`, // https://example.com/Oferta/this-is-a-post-1
+  const smakiUrls = smaki.map((smak) => ({
+    url: `${urlPrefix}/smaki/${getSmakSlug(smak)}`, // https://example.com/Oferta/this-is-a-post-1
     lastModified: new Date(), // ideally, this is the last modified date of the post
     changeFrequency: 'daily', // this will be used to determine how often pages are re-crawled
-    priority: 0.7, // the priority of this URL relative to other URLs on your site
-  }));
+    priority: 0.6, // the priority of this URL relative to other URLs on your site
+  }))
+
+
+
+  return[
+    {
+      url: urlPrefix,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1
+    },
+    {
+      url: `${urlPrefix}/smaki`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8
+    },
+    {
+      url: `${urlPrefix}/contact`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.4
+    },
+    ...smakiUrls,
+  ]
 }

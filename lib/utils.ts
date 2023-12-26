58f259fca44e39c57e99c9c2de9daf1c623f500c
Ivan Shyriaiev <ivshiryaev1999@gmail.com>
2023-12-26 02:45:06 +0100

    add slug handling functions in lib/utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,7 +5,7 @@ import slugify from 'slugify'
  * @param {string} title - the title of the post
  * @returns {string} a URL-safe slug based on the post's title
  */
-const titleToSlug = (title: string) => {
+export const titleToSlug = (title: string) => {
 	const uriSlug = slugify(title, {
 		lower: true, // convert everything to lower case
 		trim: true, // remove leading and trailing spaces
@@ -13,4 +13,35 @@ const titleToSlug = (title: string) => {
 
 	// encode special characters like spaces and quotes to be URL-safe
 	return encodeURI(uriSlug)
+}
+
+// given a post, return its slug
+export const getSmakSlug = (post) => {
+	return `${titleToSlug(post.title)}-${post.id}`
+}
+
+// given any slug, try to extract an id from it
+export const getIdFromSlug = (slug: string) => slug.split('-').pop()
+
+/**
+ * Converts slug into readable Format
+ * @param {string} inputString - slug
+ * @returns {string} a readable string
+ */
+export const convertSlugToReadableFormat = (inputString) => {
+	console.log('convertSlugToReadableFormat')
+
+	//Remove the whitespace around
+	inputString = inputString.trim()
+
+	// Capitalize first letter
+	inputString = inputString.charAt(0).toUpperCase() + inputString.slice(1)
+
+	// Split the input string by hyphen
+	const words = inputString.split('-')
+
+	const returnValue = words.join(' ')
+	console.log(returnValue)
+
+	return words.join(' ')
 }

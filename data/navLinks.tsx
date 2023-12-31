9a567b541f90be49ca736a2a3ca246029841b512
Ivan Shyriaiev <ivshiryaev1999@gmail.com>
2023-12-31 19:02:49 +0100

    changed navLinks.json to .tsx (made it to include <Icon> in each link)

---
+++ data/navLinks.tsx
@@ -0,0 +1,18 @@
+import { IoMailOutline } from "react-icons/io5"
+import { MdOutlineMenu, MdClose } from "react-icons/md"
+import { PiBeerBottleThin } from "react-icons/pi"
+
+const links = [
+	{
+		"title":"Smaki",
+		"href":"smaki",
+		"icon":<PiBeerBottleThin/>
+	},
+	{
+		"title":"Skontaktuj się",
+		"href":"contact",
+		"icon":<IoMailOutline/>
+	}
+]
+
+export default links

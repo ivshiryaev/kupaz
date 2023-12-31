51477ef5127d5bd955a185da19dfcaec713dd71c
Ivan Shyriaiev <ivshiryaev1999@gmail.com>
2023-12-31 19:07:39 +0100

    added mobile swiper to the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,12 +6,14 @@ import HowTo from '@/components/Sections/HowTo'
 import Contact from '@/components/Sections/Contact'
 import Promotions from '@/components/Sections/Promotions'
 import InfiniteScrollingRow from '@/components/Sections/InfiniteScrollingRow'
+import MobileSwiperSection from '@/components/Sections/MobileSwiperSection'
 
 export default function Home() {
     return (
         <>
             <Hero/>
             <About/>
+            <MobileSwiperSection/>
             <Promotions/>
             <Bestsellers/>
             <FAQ heading='h3'/>
